c7789c22e3455de5879c5e986f0d4a765f7cb74b
Golf518 <uristan92@gmail.com>
2022-10-18 20:52:51 +0600

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("hellowworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("mur");
+        Cat catBean1 = (Cat) applicationContext.getBean("mur");
+
+        System.out.println(bean == bean1);
+        System.out.println(catBean == catBean1);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String messege;
+
+    public void setMessege(String messege) {
+        this.messege = messege;
+    }
+
+    public String getVoic() { return messege; }
+
+
+
+
+}
